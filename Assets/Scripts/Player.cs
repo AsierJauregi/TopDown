84b6f78a4f650ae0cd5237c939fd9f78ec59e342
Asier Jauregui Rivera <72439067+AsierJauregi@users.noreply.github.com>
2025-02-16 22:24:58 +0100

    Lógica de interacción + Recoger Items a Inventario

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,13 @@ public class Player : MonoBehaviour
     private float inputV;
     private bool moviendo;
     private Vector3 puntoDestino;
+    private Vector3 puntoInteraccion;
+    private Vector3 ultimoInput;
+    private Collider2D colliderDelante;
+
     [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float radioInteraccion;
+    [SerializeField] private LayerMask queEsColisionable;
 
     // Start is called before the first frame update
     void Start()
@@ -20,21 +27,65 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        inputH = Input.GetAxis("Horizontal");
-        inputV = Input.GetAxis("Vertical");
-        if(!moviendo && (inputH != 0 || inputV != 0)) StartCoroutine(Mover());
+        LecturaInputs();
+
+        if (!moviendo && (inputH != 0 || inputV != 0))
+        {
+            ultimoInput = new Vector3(inputH, inputV, 0);
+            puntoDestino = transform.position + ultimoInput;
+            puntoInteraccion = puntoDestino;
+
+            colliderDelante = LanzarCheck();
+
+            if (!colliderDelante)
+            {
+                StartCoroutine(Mover());
+            }
+
+        }
+    }
+
+    private void LecturaInputs()
+    {
+        if (inputV == 0) inputH = Input.GetAxisRaw("Horizontal");
+        if (inputH == 0) inputV = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.E)) LanzarInteraccion();
+
+    }
+
+    private void LanzarInteraccion()
+    {
+        colliderDelante = LanzarCheck();
+        if (colliderDelante)
+        {
+            if(colliderDelante.TryGetComponent(out Interactuable interactuable))
+            {
+                interactuable.Interactuar();
+            }
+            
+        }
     }
 
     IEnumerator Mover()
     {
         moviendo = true;
-        puntoDestino = transform.position + new Vector3(inputH, inputV, 0);
-
         while (transform.position != puntoDestino)
         {
             transform.position = Vector3.MoveTowards(transform.position, puntoDestino, velocidadMovimiento * Time.deltaTime);
             yield return null;
         }
+        //Refrescar punto de interacción al terminar de movernos
+        puntoInteraccion = transform.position + ultimoInput;
         moviendo = false;
+    }
+
+    private Collider2D LanzarCheck()
+    {
+        return Physics2D.OverlapCircle(puntoInteraccion, radioInteraccion, queEsColisionable);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(puntoInteraccion, radioInteraccion);
     }
 }
--- Assets/Scripts/SistemaInventario.cs
+++ Assets/Scripts/SistemaInventario.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,7 @@ public class SistemaInventario : MonoBehaviour
 {
     [SerializeField] private GameObject marcoInventario;
     [SerializeField] private Button[] botones;
+    [SerializeField] private int itemsDisponibles = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,17 @@ public class SistemaInventario : MonoBehaviour
     private void BotonClickado(int indiceBoton)
     {
         Debug.Log("Botón Clickado " + indiceBoton);
+    }
+
+    public void NuevoItem(ItemSO datosItem)
+    {
+        //Activar boton del inventario al tener nuevo item
+        botones[itemsDisponibles].gameObject.SetActive(true);
+        //Meter al botón los datos del item
+        botones[itemsDisponibles].GetComponent<Image>().sprite = datosItem.icono;
+        botones[itemsDisponibles].gameObject.GetComponentInChildren<TextMeshProUGUI>().SetText(datosItem.nombre);
+        botones[itemsDisponibles].gameObject.GetComponentInChildren<TextMeshProUGUI>().fontSize = 14;
+        itemsDisponibles++;
     }
 
     // Update is called once per frame
